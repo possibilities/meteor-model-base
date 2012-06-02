5efd0b10062a8623bd0d73992d10581fabf4fc31
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-02 06:50:17 +0200

    allow extensions to have an initialize method

--- src/model-base/common.js
+++ src/model-base/common.js
@@ -1,14 +1,41 @@
 Model = {
-  extend: function(inheritFrom) {
+  extend: function(attributes) {
+    var self = this;
+
+    // This is the prototype for all models
     var modelClass = function(attributes) {
+
+      // Mix in the attributes
       _.extend(this, attributes);
+
+      // Boot up all the extensions
+      this._initializeExtensions();
     };
-    _.extend(modelClass.prototype, Model.prototype, inheritFrom);
+    
+    // Call initialize on any installed extensions
+    modelClass.prototype._initializeExtensions = function() {
+      var self = this;
+
+      _.each(Model._extensions, function(extension) {
+        if (extension.initialize) {
+          extension.initialize.call(self, attributes);
+        }
+      });
+    };
+    
+    // Extend all models with the child class attributes
+    _.extend(modelClass.prototype, Model.prototype, attributes);
+
+    // Extend all the models with any installed extension methods
     _.each(this._extensions, function(methods) {
       _.extend(modelClass.prototype, methods);
     });
+
+    // Done! Time to play!
     return modelClass;
   },
+
+  // Install an extension
   extension: function(extension) {
     this._extensions || (this._extensions = []);
     this._extensions.push(extension);
